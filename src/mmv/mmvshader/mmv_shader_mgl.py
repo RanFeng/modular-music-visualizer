e0372fd1436e129c3ca2a170886e50cf6690d5a0
Tremeschin <tremeschin@protonmail.com>
2021-01-22 22:17:22 -0400

    Comment and explain MMVShaderMGL

--- src/mmv/mmvshader/mmv_shader_mgl.py
+++ src/mmv/mmvshader/mmv_shader_mgl.py
@@ -32,6 +32,7 @@ from PIL import Image
 import subprocess
 import moderngl
 import logging
+import shutil
 import cv2
 import sys
 import re
@@ -117,14 +118,31 @@ class MMVShaderMGL:
             # ]
         ))
 
-        # Info we send to the shaders
+        # Info we send to the shaders read only (uniforms)
         self.pipeline = {
             "mmv_time": 0,
             "mmv_frame": 0,
             "mmv_resolution": [0, 0],
         }
+
+        # A dictionary that will hold indexes representing the location of textures
+        # as well as the object we get it from:
+        #
+        # - Is it a static image? Do nothing just use its texture
+        # - Is it a video? We need to get the next frame then upload to the GPU the texture
+        # - Is it a shader? We need to render it
+        # 
+        # This dictionary is mostly for information on which loader we have and to organize its
+        # object if we need to do some action.
         self.textures = {}
+
+        # This next list holds instances of this very own class, MMVShaderMGL,
+        # they are shaders the user mapped and we render them separately to a FBO
+        # so we can use that as a texture <here> on this main class's shader
         self.shaders_as_textures = []
+
+        # Initialize None values
+        (self.width, self.height, self.fps) = [None]*3
 
     # Configurate how we'll output the shader
     def render_config(self, width, height, fps):
@@ -132,13 +150,34 @@ class MMVShaderMGL:
         (self.width, self.height, self.fps) = (width, height, fps)
         
     # Create one FBO which is a texture under the hood so we can utilize it in some other shader
+    # Returns [texture, fbo], which the texture is attached to the fbo with the previously
+    # configured width, height
     def construct_texture_fbo(self):
+        debug_prefix = "[MMVShaderMGL.construct_texture_fbo]"
+
+        # Error assertion, width height or fps is not set
+        assert not any([value is None for value in [self.width, self.height]]), (
+            "Width or height wasn't set / is None, did you call .render_config(width, height, fps) first?"
+        )
+        
+        # Log action
+        logging.info(f"{debug_prefix} Constructing an FBO attached to an Texture with [width={self.width}] [height=[{self.height}]")
+
+        # Create a RGBA texture of this class's configured resolution
         texture = self.gl_context.texture((self.width, self.height), 4)
+
+        # Create an FBO which is attached to that previous texture, whenever we render something
+        # to this FBO after fbo.use()-ing it, the contents will be written directly on that texture
+        # which we can bind to some location in other shader. This process is recursive
         fbo = self.gl_context.framebuffer(color_attachments = [texture])
+
+        # Return the two created objects
         return [texture, fbo]
 
     # Create one context's program out of a frag and vertex shader, those are used together
-    # with VAOs so we render to some FBO
+    # with VAOs so we render to some FBO. 
+    # Parses for #pragma map name=loader:value:WxH and creates the corresponding texture, binds to it
+    # Also copies #pragma include <path.glsl> to this fragment shader
     def construct_shader(self, fragment_shader = DEFAULT_FRAGMENT_SHADER, vertex_shader = DEFAULT_VERTEX_SHADER):
         debug_prefix = "[MMVShaderMGL.construct_shader]"
 
@@ -152,26 +191,29 @@ class MMVShaderMGL:
         logging.info(f"{debug_prefix} Parsing the fragment shader for every #pragma map")
 
         # Regular expression to match #pragma map name=loader:/path/value;512x512
-        # the ;512x512 is optional and only used with the shader loader
+        # the ;512x512 is required for the image, video and shader loaders
         regex = r"#pragma map ([\w]+)=([\w]+):([\w/. -]+):?([0-9]+)?x?([0-9]+)?"
         found = re.findall(regex, fragment_shader)
 
-        # The static uniforms we'll assign the values
+        # The static uniforms we'll assign the values to (eg. image, video, shader resolutions)
         assign_static_uniforms = []
 
         # For each mapping
         for mapping in found:
+            # Get the values we matched
             name, loader, value, width, height = mapping
             logging.info(f"{debug_prefix} Matched mapping [name={name}] [loader={loader}] [width={width}] [height={height}]")
 
             # Error assertion, valid loader and path
-            loaders = ["image", "video", "shader", "use"]
+            loaders = ["image", "video", "shader", "include"]
             assert loader in loaders, f"Loader not implemented in loaders {loaders}"
             assert os.path.exists(value), f"Value of loader [{value}] is not a valid path"
 
-            # We'll map one texture
+            # We'll map one texture to this shader, either a static image, another shader or a video
+            # we do create and store the shader and video objects so we render or get the next image later on before using
             if loader in ["image", "shader", "video"]:
-                print(found)
+
+                # We need an target render size
                 assert (width != '') and (height != ''), "Width or height shouldn't be null, set WxH on pragma map with ;512x512"
 
                 # Image loader
@@ -180,28 +222,46 @@ class MMVShaderMGL:
                     img = Image.open(value).convert("RGBA")
                     width, height = img.size
 
-                    # Upload the texture to the GPU
+                    # Convert image to bytes and upload the texture to the GPU
                     logging.info(f"{debug_prefix} Uploading texture to the GPU")
                     texture = self.gl_context.texture((width, height), 4, img.tobytes())
 
+                    # Assign the name, type and texture to the textures dictionary
                     self.textures[len(self.textures.keys()) + 1] = [name, "texture", texture]
                     
                 # Add shader as texture element
                 elif loader == "shader":
 
+                    # Read the shader we'll map
                     with open(value, "r") as f:
                         loader_frag_shader = f.read()
 
+                    # Create one instance of this very own class
                     shader_as_texture = MMVShaderMGL()
+
+                    # Set the render configuration on the #pragma map for width, height. We don't need fps
+                    # as that one will receive the main class's pipeline
                     shader_as_texture.render_config(width = int(width), height = int(height), fps = None)
+
+                    # Create a texture and fbo for this mapped shader, here is where we render to the fbo
+                    # which is attached to the texture, and we use that texture in this main class
                     texture, fbo = shader_as_texture.construct_texture_fbo()
+
+                    # Construct the shader we loaded
                     shader_as_texture.construct_shader(fragment_shader = loader_frag_shader)
+
+                    # Append to the shaders as textures. We only do this for passing a pipeline to the mapped shader
                     self.shaders_as_textures.append(shader_as_texture)
 
+                    # Assign the name, type and texture to the textures dictionary
                     self.textures[len(self.textures.keys()) + 1] = [name, "shader", shader_as_texture.texture]
 
+                # Video loader
                 elif loader == "video":
+                    # Get one VideoCapture
                     video = cv2.VideoCapture(value)
+
+                    # Assign the name, type and texture to the textures dictionary
                     self.textures[len(self.textures.keys()) + 1] = [name, "video", video]
                     
                 # Add the texture uniform values
@@ -221,20 +281,25 @@ class MMVShaderMGL:
 
         # Get #pragma includes
 
+        # Simple rexed and get every match with findall
         regex = r"#pragma include ([\w/. -]+)"
         found = re.findall(regex, fragment_shader)
 
-        # For each mapping
+        # For each #pragma include we found
         for include in found:
+
+            # This will replace this line (we build back)
             replaces = f"#pragma include {include}"
             
+            # Error assertion, the path must exist
             assert os.path.exists(include), f"Value of #pragma include is not a valid path [{include}]"
 
+            # Read the other shader data
             with open(include, "r") as f:
                 include_other_glsl_data = f.read()
 
+            # Replace the line on this fragment shader with the included other one
             fragment_shader = fragment_shader.replace(replaces, include_other_glsl_data)
-            
 
         # We parsed the body and added to the prefix stuff that was defined so merge everything together
         fragment_shader = f"{fragment_shader_prefix}\n{fragment_shader}"
@@ -251,14 +316,16 @@ class MMVShaderMGL:
 
         # Assign the uniforms to blank value
         for name, value in assign_static_uniforms:
-            print(name, value)
             uniform = self.program.get(name, FakeUniform())
             uniform.value = value
 
-        import shutil
+        # Pretty print / log
         s = "-" * shutil.get_terminal_size()[0]
-        print(f"{s}\n{fragment_shader}")
-        print(f"{s}\n{vertex_shader}\n{s}")
+        print(s)
+        logging.info(f"{debug_prefix} Fragment shader is:\n{fragment_shader}")
+        print(s)
+        logging.info(f"{debug_prefix} Vertex shader is:\n{vertex_shader}")
+        print(s)
 
     # Pipe a pipeline to a target that have a program attribute
     def pipe_pipeline(self, pipeline, target):
@@ -327,9 +394,10 @@ class MMVShaderMGL:
 
     # Iterate through this class instance as a generator for getting the 
     def next(self, custom_pipeline = {}):
+
+        # Set the pipeline attributes
         self.pipeline["mmv_frame"] += 1
         self.pipeline["mmv_time"] = round((self.pipeline["mmv_frame"] / self.fps), 3)
-
         self.pipeline["mmv_resolution"] = (self.width, self.height)
 
         # Assign user custom pipelines
@@ -339,5 +407,6 @@ class MMVShaderMGL:
         # print(self.pipeline)
         self.render(pipeline = self.pipeline)
             
+    # Read from the current FBO
     def read(self):
         return self.fbo.read()
